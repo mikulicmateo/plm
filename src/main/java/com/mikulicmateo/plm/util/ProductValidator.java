eaf2eb98d08f3c5074852b5d984e6dad3a502b92
Mateo Mikulic <mikulic.dev@gmail.com>
2022-03-24 17:11:31 +0100

    Removed sout

--- src/main/java/com/mikulicmateo/plm/util/ProductValidator.java
+++ src/main/java/com/mikulicmateo/plm/util/ProductValidator.java
@@ -5,7 +5,7 @@ import com.mikulicmateo.plm.dto.request.RequestProductDto;
 public class ProductValidator {
 
     public static boolean validateProduct(RequestProductDto productDto){
-        System.out.println(productDto + " " + productDto.getCode() + " " + productDto.getPriceHrk() + " " + productDto.getCode().length());
+
         if(productDto == null || productDto.getCode() == null || productDto.getPriceHrk() < 0 || productDto.getCode().length() != 10)
             return false;
 
