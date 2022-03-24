1d3e371345add508e0d8c881cdea43749ac966fa
Mateo Mikulic <mikulic.dev@gmail.com>
2022-03-24 17:10:32 +0100

    Update product

--- src/main/java/com/mikulicmateo/plm/service/ProductService.java
+++ src/main/java/com/mikulicmateo/plm/service/ProductService.java
@@ -5,6 +5,8 @@ import com.mikulicmateo.plm.dto.response.ResponseMessageDto;
 import com.mikulicmateo.plm.entity.Product;
 import com.mikulicmateo.plm.mapper.ProductMapper;
 import com.mikulicmateo.plm.repository.ProductRepository;
+import com.mikulicmateo.plm.util.CurrencyClient;
+import com.mikulicmateo.plm.util.ProductValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,7 +25,7 @@ public class ProductService {
 
     @Transactional
     public ResponseMessageDto saveProduct(RequestProductDto productDto) {
-        if(validateNewProduct(productDto)){
+        if(ProductValidator.validateProduct(productDto)){
 
             if(productRepository.findByCode(productDto.getCode()).isPresent()){
                 return new ResponseMessageDto(false, "Product already exists.");
@@ -38,11 +40,31 @@ public class ProductService {
         }
     }
 
-    private boolean validateNewProduct(RequestProductDto productDto){
-        System.out.println(productDto + " " + productDto.getCode() + " " + productDto.getPriceHrk() + " " + productDto.getCode().length());
-        if(productDto == null || productDto.getCode() == null || productDto.getPriceHrk() < 0 || productDto.getCode().length() != 10)
-            return false;
+    @Transactional
+    public ResponseMessageDto updateProduct(long id, RequestProductDto productDto) {
+        if(ProductValidator.validateProduct(productDto)){
+            Product productToUpdate = productRepository.getById(id);
+            if(productToUpdate == null){
+                return new ResponseMessageDto(false, "Product does not exist.");
+            }
+            productRepository.save(updateProductFields(productToUpdate, productDto));
 
-        return true;
+            return new ResponseMessageDto(true, "Product successfully updated.");
+        }
+        else{
+            return new ResponseMessageDto(false, "Product not correctly defined.");
+        }
+
     }
+
+    private Product updateProductFields(Product product, RequestProductDto productDto){
+        product.setCode(productDto.getCode());
+        product.setName(productDto.getName());
+        product.setPriceHrk(productDto.getPriceHrk());
+        product.setPriceEur(productDto.getPriceHrk() / CurrencyClient.getEurCurrency());
+        product.setDescription(productDto.getDescription());
+        product.setAvailable(productDto.isAvailable());
+        return product;
+    }
+
 }
